82b034af34874699d269f8a33aedd07594625d57
Ben <ben.fu0413@gmail.com>
2022-11-02 16:02:10 +0800

    [games101] homewrok0

--- Games101/code/pa0/main.cpp
+++ Games101/code/pa0/main.cpp
@@ -3,6 +3,51 @@
 #include"eigen3/Eigen/Dense"
 #include<iostream>
 
+// 给定一个点 P=(2,1), 将该点绕原点先逆时针旋转 45◦，再平移 (1,2), 计算出 
+// 变换后点的坐标（要求用齐次坐标进行计算
+static void homework0(Eigen::Vector3f v);
+
+void homework0(Eigen::Vector3f v)
+{   
+    // Transform
+    // 1 0 1   x   x+1
+    // 0 1 2 * y = y+2
+    // 0 0 1   1   1
+
+    // Rotate
+    // cos(a)   -sin(a)     0     x
+    // sin(a)   cos(a)      0   * y
+    // 0        0           1     z
+
+    // 1 0 1   x   x+1
+    // 0 1 2 * y = y+2
+    // 0 0 1   1   1
+
+    Eigen::Matrix3f translate, rotate;
+
+    translate << 1.0, 0, 1.0, 0, 1.0, 2.0, 0, 0, 1.0;
+
+    rotate << std::cos(45.0/180.0*acos(-1)), -std::sin(45.0/180.0*acos(-1)), 0, std::sin(45.0/180.0*acos(-1)), std::cos(45.0/180.0*acos(-1)), 0, 0, 0, 1;
+
+    Eigen::Matrix3f trans = translate * rotate;
+
+    Eigen::Vector3f result = trans * v;
+
+    std::cout << "homework0" << std::endl;
+    std::cout << "rotate" << std::endl;
+    std::cout << rotate << std::endl;
+
+    std::cout << "translate" << std::endl;
+    std::cout << translate << std::endl;
+
+    std::cout << "trans" << std::endl;
+    std::cout << trans << std::endl;
+
+    std::cout << "result" << std::endl;
+    std::cout << result << std::endl;
+
+}
+
 int main(){
 
     // Basic Example of cpp
@@ -43,6 +88,32 @@ int main(){
     // matrix scalar multiply i * 2.0
     // matrix multiply i * j
     // matrix multiply vector i * v
+
+    std::cout << "Example of Matrix Add" << std::endl;
+    std::cout << i + j << std::endl;
+    // 3 5 4 
+    // 8 11 11
+    // 16 15 17
+
+    std::cout << "Example of Matrix scalar multiply i * 2.0" << std::endl;
+    std::cout << i * 2.0 << std::endl;
+    // 2,4,6,8,10,12,14,16,18
+
+    std::cout << "Example of Matrix multiply i * j" << std::endl;
+    std::cout << i * j << std::endl;
+    // 1 2 3   2 3 1   37  36  35
+    // 4 5 6 * 4 6 5 = 82  84  77
+    // 7 8 9   9 7 8   127 132 119
+
+    std::cout << "Example of Matrix multiply vector i * v" << std::endl;
+    std::cout << i * v << std::endl;
+    // 1 2 3   1   14
+    // 4 5 6 * 2 = 32 
+    // 7 8 9   3   50
+
+    // homework0 (2, 1, 1)
+    Eigen::Vector3f vh(2.0f,1.0f,1.0f);
+    homework0(vh);
 
     return 0;
 }
--- Games101/note/GAMES101-L05.md
+++ Games101/note/GAMES101-L05.md
@@ -8,7 +8,7 @@
   - Perspective projection (frustum to "canonical" cube)
 - Canonical cube to ?
 
-### Canonical Cube to Screen?
+### Canonical Cube to Screen? 📝
 - What is screen?
   - An array of pixels. 1920*1080 (1080P) 1080*720(720P)
   - Size of the array: resolution
@@ -17,4 +17,18 @@
   - Rasterize == drawing onto the screen
 - Pixel (FYI, short of "picture element")
   - For now: A pixel is a little square with uniform color
-  - Color is a mixture of ( red , green, blue) 
+  - Color is a mixture of ( <font color='red'> red </font>, <font color='green'> green </font>, <font color='blue'> blue </font>) 
+
+- Viewport transform matrix:
+$$
+M_{viewport} = 
+\begin{pmatrix} 
+\frac {width}{2} & 0 & 0 & \frac {width}{2} \\ 
+0 & \frac {height}{2} & 0 & \frac {height}{2} \\
+0 & 0 & 0 & 0 \\
+0 & 0 & 0 & 1
+\end{pmatrix}
+$$
+
+## Drawing Machines
+- 电子墨水瓶刷新用时比较久；
