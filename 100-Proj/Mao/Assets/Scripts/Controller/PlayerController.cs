b8d28d5c9035094e2667efad7990e2cc0f0ebf28
Ben <bei.fu@biubiux2.com>
2022-11-22 20:53:11 +0800

    【Unity】【Mao 】Animator 动画控制器

--- 100-Proj/Mao/Assets/Scripts/Controller/PlayerController.cs
+++ 100-Proj/Mao/Assets/Scripts/Controller/PlayerController.cs
@@ -7,14 +7,29 @@ public class PlayerController : MonoBehaviour
 {
     private NavMeshAgent _agent;
 
+    private Animator _animator;
+
     private void Awake()
     {
         _agent = GetComponent<NavMeshAgent>();
+
+        _animator = GetComponent<Animator>();
+    }
+
+
+    private void Update()
+    {
+        SwitchAnimation();
     }
 
     private void Start()
     {
         MouseManager.Instance.OnMouseClicked += MoveToTarget;
+    }
+
+    private void SwitchAnimation()
+    {
+        _animator.SetFloat("Speed", _agent.velocity.sqrMagnitude);
     }
 
     public void MoveToTarget(Vector3 target)
