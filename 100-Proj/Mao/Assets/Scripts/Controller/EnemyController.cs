1ac933113a942be9b4b63c202bf9bb0e48a935ab
Ben <bei.fu@biubiux2.com>
2022-11-23 19:36:54 +0800

    【Unity】【Mao 】Enemy Animator Controller；Enemy Patorl State & Guide ~

--- 100-Proj/Mao/Assets/Scripts/Controller/EnemyController.cs
+++ 100-Proj/Mao/Assets/Scripts/Controller/EnemyController.cs
@@ -19,29 +19,76 @@ public class EnemyController : MonoBehaviour
 
     private EnemyState _enemyState;
 
-    private GameObject _player;
+    private GameObject _attackTarget;
+
+    private Animator _animator;
 
 
     [Header("Basic Setting")]
 
     public float SightRadius;
 
+    public bool isGuard;
+
+    private float _speed;
+
+    public float LookAtTime;
+    private float _remainLookAtTime;
+
+    //
+    bool _isWalk;
+
+    bool _isChase;
+
+    bool _isFollow;
+
+    [Header("Patrol Setting")]
+    public float PatrolRange;
+
+    private Vector3 _wayPoint;
+
+    private Vector3 _guidePos;
+
     private void Awake()
     {
         _agent = GetComponent<NavMeshAgent>();
+
+        _animator = GetComponent<Animator>();
+
+        _speed = _agent.speed;
+
+        _guidePos = transform.position;
+
+        _remainLookAtTime = LookAtTime;
+    }
+
+    private void Start()
+    {
+        if (isGuard)
+        {
+            _enemyState = EnemyState.GUARD;
+        }
+        else
+        {
+            _enemyState = EnemyState.PATROL;
+            GetNewWayPoint();
+        }
     }
 
     private void Update()
     {
         SwitchState();
+
+        SwitchAnimation();
     }
 
     private void SwitchState()
     {
-        if (FoundPlayer())
+        bool foundPlayer = FoundPlayer();
+
+        if (foundPlayer)
         {
             _enemyState = EnemyState.CHASE;
-            Debug.Log("Found Player");
         }
 
         switch (_enemyState)
@@ -53,12 +100,59 @@ public class EnemyController : MonoBehaviour
                 break;
             case EnemyState.PATROL:
                 {
+                    _isChase = false;
+                    _agent.speed = _speed * 0.5f;
 
+                    if (Vector3.Distance(_wayPoint, transform.position) < _agent.stoppingDistance)
+                    {
+                        _isWalk = false;
+
+                        if (_remainLookAtTime > 0)
+                        {
+                            _remainLookAtTime -= Time.deltaTime;
+                        }
+                        else
+                        {
+                            GetNewWayPoint();
+                        }
+                    }
+                    else
+                    {
+                        _isWalk = true;
+                        _agent.destination = _wayPoint;
+                    }
                 }
                 break;
             case EnemyState.CHASE:
                 {
+                    _isWalk = false;
+                    _isChase = true;
 
+                    if (foundPlayer)
+                    {
+                        _isFollow = true;
+                        if (_remainLookAtTime > 0)
+                        {
+                            _agent.destination = _attackTarget.transform.position;
+                            _remainLookAtTime -= Time.deltaTime;
+                        }
+                        else
+                        {
+                            if (isGuard)
+                            {
+                                _enemyState = EnemyState.GUARD;
+                            }
+                            else
+                            {
+                                _enemyState = EnemyState.PATROL;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        _isFollow = false;
+                        _agent.destination = transform.position;
+                    }
                 }
                 break;
             case EnemyState.DEAD:
@@ -70,20 +164,47 @@ public class EnemyController : MonoBehaviour
         }
     }
 
+    private void SwitchAnimation()
+    {
+        _animator.SetBool("Walk", _isWalk);
+        _animator.SetBool("Chase", _isChase);
+        _animator.SetBool("Follow", _isFollow);
+    }
+
     private bool FoundPlayer()
     {
         var colliders = Physics.OverlapSphere(transform.position, SightRadius);
 
         foreach (var collider in colliders)
         {
-            if (collider.gameObject.CompareTag("Player"))
+            if (collider.CompareTag("Player"))
             {
-                _player = collider.gameObject;
+                _attackTarget = collider.gameObject;
 
                 return true;
             }
         }
 
         return false;
+    }
+
+    void GetNewWayPoint()
+    {
+        _remainLookAtTime = LookAtTime;
+
+        float randomX = Random.Range(-PatrolRange, PatrolRange);
+        float randomZ = Random.Range(-PatrolRange, PatrolRange);
+
+        Vector3 randomPoint = new Vector3(_guidePos.x + randomX, transform.position.y, _guidePos.z + randomZ);
+
+        // FIX
+        NavMeshHit hit;
+        _wayPoint = NavMesh.SamplePosition(randomPoint, out hit, PatrolRange, 1) ? hit.position : transform.position;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, SightRadius);
     }
 }
